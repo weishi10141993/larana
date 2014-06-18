10ada717138334666c18de01fa377fbc3e7c4586
Wesley Ketchum <wketchum@fnal.gov>
2014-06-18 10:29:14 -0500

    first stab at testing for AlgoThreshold

---
+++ test/OpticalDetector/AlgoThreshold_test.cc
@@ -0,0 +1,13 @@
+#define BOOST_TEST_MODULE ( AlgoThreshold_test )
+#include "boost/test/auto_unit_test.hpp"
+
+BOOST_AUTO_TEST_SUITE(AlgoThreshold_test)
+
+BOOST_AUTO_TEST_CASE(checkError){ BOOST_FAIL("This test fails"); }
+
+//bool test(const std::vector<uint16_t> *wf){
+//
+//}
+
+
+BOOST_AUTO_TEST_SUITE_END()
