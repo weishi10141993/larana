c6f6afe935b660af1687babeffce852fa7f07935
Wesley Ketchum <wketchum@fnal.gov>
2015-01-15 16:41:38 -0600

    minor compiling fixes, with updating Cmake commands to make extra FlashHypothesis library

--- OpticalDetector/FlashHypothesisAlg.cxx
+++ OpticalDetector/FlashHypothesisAlg.cxx
@@ -23,43 +23,45 @@ opdet::FlashHypothesisAlg::GetFlashHypothesisCollection(recob::Track const& trac
 
   FlashHypothesisCollection fhc(geom.NOpDet());
   for(size_t pt=1; pt<track.NumberTrajectoryPoints(); pt++)
-    fhc = fhc + _alg.CreateFlashHypothesesFromSegment(track.LocationAtPoint(pt-1),
-						      track.LocationAtPoint(pt),
-						      dEdxVector[pt],
-						      geom,pvs,larp,opdigip,XOffset);
+    fhc = fhc + CreateFlashHypothesesFromSegment(track.LocationAtPoint(pt-1),
+						 track.LocationAtPoint(pt),
+						 dEdxVector[pt],
+						 geom,pvs,larp,opdigip,XOffset);
+  return fhc;
 }
 
 opdet::FlashHypothesisCollection 
-opdet::FlashHypothesisCollection::FlashHypothesisCollection(std::vector<TVector3> const& trajVector, 
-							    std::vector<float> dEdxVector,
-							    geo::Geometry const& geom,
-							    phot::PhotonVisibilityService const& pvs,
-							    util::LArProperties const& larp,
-							    opdet::OpDigiProperties const& opdigip,
-							    float XOffset)
+opdet::FlashHypothesisAlg::GetFlashHypothesisCollection(std::vector<TVector3> const& trajVector, 
+							std::vector<float> dEdxVector,
+							geo::Geometry const& geom,
+							phot::PhotonVisibilityService const& pvs,
+							util::LArProperties const& larp,
+							opdet::OpDigiProperties const& opdigip,
+							float XOffset)
 {
   if(trajVector.size() != dEdxVector.size())
     throw "ERROR in FlashHypothesisCollection: dEdx vector size not same as track size.";
 
   FlashHypothesisCollection fhc(geom.NOpDet());
   for(size_t pt=1; pt<trajVector.size(); pt++)
-    fhc = fhc + _alg.CreateFlashHypothesesFromSegment(trajVector[pt-1],
-						      trajVector[pt],
-						      dEdxVector[pt],
-						      geom,pvs,larp,opdigip,XOffset);
+    fhc = fhc + CreateFlashHypothesesFromSegment(trajVector[pt-1],
+						 trajVector[pt],
+						 dEdxVector[pt],
+						 geom,pvs,larp,opdigip,XOffset);
 
+  return fhc;
 }
 
 opdet::FlashHypothesisCollection 
-opdet::FlashHypothesisCollection::GetFlashHypothesisCollection(TVector3 const& pt1, TVector3 const& pt2, 
-							       float dEdx,
-							       geo::Geometry const& geom,
-							       phot::PhotonVisibilityService const& pvs,
-							       util::LArProperties const& larp,
-							       opdet::OpDigiProperties const& opdigip,
-							       float XOffset)
+opdet::FlashHypothesisAlg::GetFlashHypothesisCollection(TVector3 const& pt1, TVector3 const& pt2, 
+							float dEdx,
+							geo::Geometry const& geom,
+							phot::PhotonVisibilityService const& pvs,
+							util::LArProperties const& larp,
+							opdet::OpDigiProperties const& opdigip,
+							float XOffset)
 {
-  return _alg.CreateFlashHypothesesFromSegment(pt1,pt2,dEdx,geom,pvs,larp,opdigip,XOffset);
+  return CreateFlashHypothesesFromSegment(pt1,pt2,dEdx,geom,pvs,larp,opdigip,XOffset);
 }
 
 opdet::FlashHypothesisCollection
@@ -71,26 +73,27 @@ opdet::FlashHypothesisAlg::CreateFlashHypothesesFromSegment(TVector3 const& pt1,
 							    opdet::OpDigiProperties const& opdigip,
 							    float XOffset)
 {
+  FlashHypothesisCollection fhc(geom.NOpDet());
+
   FlashHypothesis prompt_hyp = FlashHypothesis(geom.NOpDet());
   
   std::vector<double> xyz_segment(_calc.SegmentMidpoint(pt1,pt2,XOffset));
   
   //get the visibility vector
-  const std::vector<float>* PointVisibility = pvs.GetAllVisibilities(xyz_segment[0]);
+  const std::vector<float>* PointVisibility = pvs.GetAllVisibilities(&xyz_segment[0]);
   
   //check vector size, as it may be zero if given a y/z outside some range
-  if(PointVisibility->size()!=geom.NOpDet()) return;
+  if(PointVisibility->size()!=geom.NOpDet()) return fhc;
   
   //klugey ... right now, set a qe_vector that gives constant qe across all opdets
   std::vector<float> qe_vector(geom.NOpDet(),opdigip.QE());
-  _calc.FillFlashHypotheses(larp.ScintYield()*larp.ScientYieldRatio(),
+  _calc.FillFlashHypothesis(larp.ScintYield()*larp.ScintYieldRatio(),
 			    dEdx,
 			    pt1,pt2,
 			    qe_vector,
 			    *PointVisibility,
 			    prompt_hyp);
   
-  FlashHypothesisCollection fhc;
-  fhc.SetPromptHypAndPromptFrac(prompt_hyp,larp.ScientYieldRatio());
+  fhc.SetPromptHypAndPromptFraction(prompt_hyp,larp.ScintYieldRatio());
   return fhc;
 }
