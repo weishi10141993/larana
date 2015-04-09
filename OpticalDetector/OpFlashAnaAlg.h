06c6f688c9a1e0682f3a8dd0688ad962eb7ee401
Wesley Ketchum <wketchum@fnal.gov>
2015-04-09 10:41:33 -0500

    add in files for standalong OpFlashAnaAlg

---
+++ OpticalDetector/OpFlashAnaAlg.h
@@ -0,0 +1,26 @@
+#ifndef OPFLASHANAALG_H
+#define OPFLASHANAALG_H
+
+/*!
+ * Title:   OpFlashAnaAlg Class
+ * Author:  Wes Ketchum (wketchum@lanl.gov)
+ *
+ * Description: 
+ * Alg that stores flash information in a handy tree.
+ * 
+*/
+
+#include "RecoBase/OpFlash.h"
+
+#include "TTree.h"
+
+namespace opdet{
+
+  class OpFlashAnaAlg{
+
+  };
+  
+}
+
+
+#endif
