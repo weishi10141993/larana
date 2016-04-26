ea3e6405c89d7d536e1ba37476db3d30bd7ee456
mdhaigh <m.haigh@warwick.ac.uk>
2016-04-26 11:33:17 +0100

    Add some variables to MVA PID and code to allow cheating of track sense based on truth vertex.

--- larana/ParticleIdentification/MVAPID_module.cc
+++ larana/ParticleIdentification/MVAPID_module.cc
@@ -57,6 +57,7 @@ namespace mvapid {
   this->reconfigure(pset);
   produces< std::vector<anab::MVAPIDResult> >();
   produces< art::Assns<recob::Track, anab::MVAPIDResult, void> >();
+  produces< art::Assns<recob::Shower, anab::MVAPIDResult, void> >();
   fResult=new std::vector<anab::MVAPIDResult>;
 }
 
@@ -74,10 +75,8 @@ MVAPID::~MVAPID()
 // ***************** //
 void MVAPID::beginJob()
 {
-
   art::ServiceHandle<art::TFileService> tfs;
   fTree =tfs->make<TTree>("MVAPID","Results");/**All-knowing tree with reconstruction information*/
-
   fTree->Branch("run",&fRun,"run/I");
   fTree->Branch("subrun",&fSubrun,"subrun/I");
   fTree->Branch("event",&fEvent,"event/I");
@@ -94,17 +93,17 @@ void MVAPID::beginRun(art::Run&)
 void MVAPID::produce(art::Event& evt)
 { 
   std::unique_ptr<std::vector<anab::MVAPIDResult> > result(new std::vector<anab::MVAPIDResult>);
-  std::unique_ptr< art::Assns<recob::Track, anab::MVAPIDResult> > assns(new art::Assns<recob::Track, anab::MVAPIDResult>);
-
+  std::unique_ptr< art::Assns<recob::Track, anab::MVAPIDResult> > trackAssns(new art::Assns<recob::Track, anab::MVAPIDResult>);
+  std::unique_ptr< art::Assns<recob::Shower, anab::MVAPIDResult> > showerAssns(new art::Assns<recob::Shower, anab::MVAPIDResult>);
   fRun = evt.id().run();
   fSubrun = evt.id().subRun();
   fEvent = evt.id().event();
-
-  fAlg.RunPID(evt,*result,*assns);
+  fAlg.RunPID(evt,*result,*trackAssns,*showerAssns);
   *fResult=*result;
   fTree->Fill();
   evt.put(std::move(result));
-  evt.put(std::move(assns));
+  evt.put(std::move(trackAssns));
+  evt.put(std::move(showerAssns));
 }
   
   DEFINE_ART_MODULE(MVAPID)
--- larana/ParticleIdentification/scripts/TrainMVA.sh
+++ larana/ParticleIdentification/scripts/TrainMVA.sh
@@ -32,6 +32,23 @@ echo "Background files are:"
 for i in $BKGFILES;do echo $i;done
 echo "Job name is $JOBNAME"
 
+#Hmm, when using mrb then $LARANA_DIR points to source tree, but
+#in installed version then need to add source to path
+if [ -d $LARANA_DIR/source ]; then
+    cp $LARANA_DIR/source/ParticleIdentification/scripts/TrainMVA.C ./
+    cp $LARANA_DIR/source/ParticleIdentification/scripts/runPID_noMVA.fcl ./
+else
+    cp $LARANA_DIR/ParticleIdentification/scripts/TrainMVA.C ./
+    cp $LARANA_DIR/ParticleIdentification/scripts/runPID_noMVA.fcl ./
+fi
+
+if [ -d $LARDATA_DIR/source ]; then
+    cp $LARDATA_DIR/source/AnalysisBase/MVAPIDResult.h ./
+else
+    cp $LARDATA_DIR/AnalysisBase/MVAPIDResult.h ./
+fi
+
+
 ##Build CINT dictionary for the MVAResult class
 echo '
 #pragma link C++ class anab::MVAPIDResult+;
@@ -39,7 +56,7 @@ echo '
 #pragma link C++ class map<string,double>+;
 ' > Linkdef.h
 
-rootcint -f MVAResultDict.cxx -c $LARDATA_DIR/source/AnalysisBase/MVAPIDResult.h Linkdef.h
+rootcint -f MVAResultDict.cxx -c $LARDATA_DIR/AnalysisBase/MVAPIDResult.h Linkdef.h
 g++ -shared -o MVAResultDict.so `root-config --ldflags` -fPIC -I$ROOTSYS/include MVAResultDict.cxx
 rm -f MVAResultDict.cxx MVAResultDict.h Linkdef.h
 
@@ -52,17 +69,22 @@ std::vector<std::string> sigFiles, bkgFiles;" > $ROOTSCRIPT
 
 for i in $SIGFILES
 do
-#lar -c runPID.fcl -s $i -n 1000
+lar -c ./runPID_noMVA.fcl -s $i -n 100000
+TUPLEFILE=ntuple_`basename $i`
+mv ntuple.root $TUPLEFILE
 echo "
-BuildTree(\"ntuple.root\",\"train_"`basename $i`"\");
+BuildTree(\"${TUPLEFILE}\",\"train_"`basename $i`"\");
 sigFiles.push_back(\"train_"`basename $i`"\");" >> $ROOTSCRIPT
 done
 
 for i in $BKGFILES
 do
-#lar -c runPID.fcl -s $i -n 1000
+mv ntuple.root ntuple_$i
+lar -c ./runPID_noMVA.fcl -s $i -n 100000
+TUPLEFILE=ntuple_`basename $i`
+mv ntuple.root $TUPLEFILE
 echo "
-BuildTree(\"ntuple.root\",\"train_"`basename $i`"\");
+BuildTree(\"${TUPLEFILE}\",\"train_"`basename $i`"\");
 bkgFiles.push_back(\"train_"`basename $i`"\");" >> $ROOTSCRIPT
 done
 
@@ -73,4 +95,4 @@ TrainMVA(sigFiles,bkgFiles,\"mvaPlots.root\",\"$JOBNAME\");
 
 cat $ROOTSCRIPT
 
-#root -l -b -q $ROOTSCRIPT
+root -l -b -q $ROOTSCRIPT
