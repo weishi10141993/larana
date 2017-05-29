34f66d6381d68af3598a42d1e897584aac281b94
Marco Del Tutto <marco.deltutto@physics.ox.ac.uk>
2017-05-29 17:58:28 -0500

    Adding sanity check before returning pedestal

--- larana/OpticalDetector/OpHitFinder/PedAlgoRmsSlider.h
+++ larana/OpticalDetector/OpHitFinder/PedAlgoRmsSlider.h
@@ -18,7 +18,6 @@
 // STL
 #include "PMTPedestalBase.h"
 #include "fhiclcpp/ParameterSet.h"
-//#include "FhiclLite/PSet.h"
 
 #include <fstream>
 
@@ -45,11 +44,6 @@ namespace pmtana
     /// Print settings
     void PrintInfo();
 
-    /// Returns the mean of the elements of the vector from start to start+nsample
-    double CalcMean(const std::vector<double>& wf, size_t start, size_t nsample);
-
-    /// Returns the std of the elements of the vector from start to start+nsample
-    double CalcStd(const std::vector<double>& wf, const double ped_mean, size_t start, size_t nsample);
 
   protected:
 
@@ -60,17 +54,26 @@ namespace pmtana
     
   private:
 
-    size_t _sample_size;
+    size_t _sample_size;  ///< How many samples are used to calculate local rms and mean
+    double _threshold;    ///< Threshold applied to local rms to claim a pulse
 
-    double _threshold;
+    float _max_sigma;     ///< Max sigma to consider adc as 'sane'
+    float _ped_range_max; ///< Max value of adc to consider adc as 'sane'
+    float _ped_range_min; ///< Min value of adc to consider adc as 'sane'
 
-    int _n_presamples;
-
-    bool _verbose;
-    int _n_wf_to_csvfile;
+    bool _verbose;        ///< For debugging
+    int _n_wf_to_csvfile; ///< If greater than zero saves firsts waveforms with pedestal to csv file
     int _wf_saved = 0;
     std::ofstream _csvfile;    
 
+    /// Returns the mean of the elements of the vector from start to start+nsample
+    double CalcMean(const std::vector<double>& wf, size_t start, size_t nsample);
+
+    /// Returns the std of the elements of the vector from start to start+nsample
+    double CalcStd(const std::vector<double>& wf, const double ped_mean, size_t start, size_t nsample);
+
+    /// Checks the sanity of the estimated pedestal, returns false if not sane
+    bool CheckSanity(pmtana::PedestalMean_t& mean_v, pmtana::PedestalSigma_t& sigma_v);
   };
 }
 #endif
