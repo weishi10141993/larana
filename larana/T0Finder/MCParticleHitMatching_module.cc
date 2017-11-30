402bc6b4c40bf629e271bdad3304eaf357744406
Usher, Tracy L <usher@slac.stanford.edu>
2017-11-30 13:06:10 -0800

    Convert the module to use the pseudo tools, allows this to switch between different methods

--- larana/T0Finder/MCParticleHitMatching_module.cc
+++ larana/T0Finder/MCParticleHitMatching_module.cc
@@ -20,15 +20,8 @@
 // Framework includes
 #include "art/Framework/Core/EDProducer.h"
 #include "art/Framework/Core/ModuleMacros.h"
-#include "canvas/Persistency/Common/FindManyP.h"
 #include "art/Framework/Principal/Handle.h"
 #include "art/Framework/Principal/Event.h" 
-#include "canvas/Persistency/Common/Ptr.h" 
-#include "canvas/Persistency/Common/PtrVector.h" 
-#include "art/Framework/Principal/Run.h"
-#include "art/Framework/Principal/SubRun.h"
-#include "art/Framework/Services/Optional/TFileService.h" 
-#include "art/Framework/Services/Optional/TFileDirectory.h"
 
 #include "canvas/Utilities/InputTag.h"
 #include "fhiclcpp/ParameterSet.h"
@@ -40,15 +33,9 @@
 #include <iterator>
 
 // LArSoft
-#include "lardataobj/RecoBase/Hit.h"
-
-#include "lardata/Utilities/AssociationUtil.h"
-#include "lardata/DetectorInfoServices/LArPropertiesService.h"
-#include "lardata/DetectorInfoServices/DetectorPropertiesService.h"
-#include "nusimdata/SimulationBase/MCParticle.h"
-#include "nusimdata/SimulationBase/MCTruth.h"
-#include "larsim/MCCheater/BackTracker.h"
-#include "lardataobj/AnalysisBase/BackTrackerMatchingData.h"
+#include "larana/T0Finder/AssociationsTools/IHitParticleAssociations.h"
+#include "larana/T0Finder/AssociationsTools/DirectHitParticleAssns_tool.h"
+#include "larana/T0Finder/AssociationsTools/IndirectHitParticleAssns_tool.h"
 
 namespace t0 {
   class MCParticleHitMatching;
@@ -74,29 +61,31 @@ public:
     void reconfigure(fhicl::ParameterSet const & p) override;
 
 private:
-  
-    art::InputTag fHitModuleLabel;
-    art::InputTag fMCParticleModuleLabel;
-
-    struct TrackIDEinfo {
-        float E;
-        float NumElectrons;
-    };
-    std::unordered_map<int, TrackIDEinfo> fTrkIDECollector;
-
+    
+    // For keeping track of the replacement backtracker
+    std::unique_ptr<IHitParticleAssociations> fHitParticleAssociations;
 };
 
 
-t0::MCParticleHitMatching::MCParticleHitMatching(fhicl::ParameterSet const & p)
+t0::MCParticleHitMatching::MCParticleHitMatching(fhicl::ParameterSet const & pset)
 {
-    reconfigure(p);
+    reconfigure(pset);
     produces< art::Assns<recob::Hit , simb::MCParticle, anab::BackTrackerHitMatchingData > > ();
 }
 
-void t0::MCParticleHitMatching::reconfigure(fhicl::ParameterSet const & p)
+void t0::MCParticleHitMatching::reconfigure(fhicl::ParameterSet const & pset)
 {
-    fHitModuleLabel = p.get<art::InputTag>("HitModuleLabel");
-    fMCParticleModuleLabel = p.get<art::InputTag>("MCParticleModuleLabel");
+    // Get the tool for MC Truth matching
+    const fhicl::ParameterSet& hitPartAssnsParams = pset.get<fhicl::ParameterSet>("HitParticleAssociations");
+    
+    if (hitPartAssnsParams.get<std::string>("tool_type") == "DirectHitParticleAssns")
+    {
+        fHitParticleAssociations = std::unique_ptr<IHitParticleAssociations>(new DirectHitParticleAssns(hitPartAssnsParams));
+    }
+    else
+    {
+        fHitParticleAssociations = std::unique_ptr<IHitParticleAssociations>(new IndirectHitParticleAssns(hitPartAssnsParams));
+    }
 }
 
 void t0::MCParticleHitMatching::beginJob()
@@ -107,83 +96,9 @@ void t0::MCParticleHitMatching::produce(art::Event & evt)
 {
     if (evt.isRealData()) return;
 
-    // Access art services...
-    art::ServiceHandle<cheat::BackTracker> bt;
-
-    auto mcpartHandle = evt.getValidHandle< std::vector<simb::MCParticle> >(fMCParticleModuleLabel);
-    std::unique_ptr< art::Assns<recob::Hit, simb::MCParticle, anab::BackTrackerHitMatchingData > > MCPartHitassn( new art::Assns<recob::Hit, simb::MCParticle, anab::BackTrackerHitMatchingData >);
-  
-    double maxe = -1;
-    double tote = 0;
-    // int    trkid = -1;
-    int    maxtrkid = -1;
-    double maxn = -1;
-    double totn = 0;
-    int maxntrkid = -1;
-    anab::BackTrackerHitMatchingData bthmd;
-    std::unordered_map<int,int> trkid_lookup; //indexed by geant4trkid, delivers MC particle location
-  
-    art::Handle< std::vector<recob::Hit> > hitListHandle;
-    evt.getByLabel(fHitModuleLabel,hitListHandle);
-  
-    if(!hitListHandle.isValid()){
-        std::cerr << "Hit handle is not valid!" << std::endl;
-        return; // violation of convention!
-    }
-  
-    auto const& hitList(*hitListHandle);
-    auto const& mcpartList(*mcpartHandle);
-
-    for(size_t i_h=0; i_h<hitList.size(); ++i_h){
-        art::Ptr<recob::Hit> hitPtr(hitListHandle, i_h);
-        auto trkide_list = bt->HitToTrackID(hitPtr);
+    std::unique_ptr<HitParticleAssociations> MCPartHitassn( new HitParticleAssociations);
     
-        maxe = -1; tote = 0; maxtrkid = -1;
-        maxn = -1; totn = 0; maxntrkid = -1;
-        fTrkIDECollector.clear();
-    
-        //for(auto const& t : trkide_list){
-        for(size_t i_t=0; i_t<trkide_list.size(); ++i_t){
-            auto const& t(trkide_list[i_t]);
-            fTrkIDECollector[t.trackID].E += t.energy;
-            tote += t.energy;
-            if(fTrkIDECollector[t.trackID].E>maxe) { maxe = fTrkIDECollector[t.trackID].E; maxtrkid = t.trackID; }
-            fTrkIDECollector[t.trackID].NumElectrons += t.numElectrons;
-            totn += t.numElectrons;
-            if(fTrkIDECollector[t.trackID].NumElectrons > maxn) {
-                maxn = fTrkIDECollector[t.trackID].NumElectrons;
-                maxntrkid = t.trackID;
-            }
-      
-            //if not found, find mc particle...
-            if(trkid_lookup.find(t.trackID)==trkid_lookup.end()){
-                size_t i_p=0;
-                while(i_p<mcpartList.size()){
-                    if(mcpartList[i_p].TrackId() == t.trackID) { trkid_lookup[t.trackID] = (int)i_p; break;}
-                    ++i_p;
-                }
-                if(i_p==mcpartList.size()) trkid_lookup[t.trackID] = -1;
-            }
-      
-        }
-        //end loop on TrackIDs
-    
-    
-        //now find the mcparticle and loop back through ...
-        for(auto const& t : fTrkIDECollector){
-            int mcpart_i = trkid_lookup[t.first];
-            if(mcpart_i==-1) continue; //no mcparticle here
-            art::Ptr<simb::MCParticle> mcpartPtr(mcpartHandle, mcpart_i);
-            bthmd.ideFraction = t.second.E / tote;
-            bthmd.isMaxIDE = (t.first==maxtrkid);
-            bthmd.ideNFraction = t.second.NumElectrons / totn;
-            bthmd.isMaxIDEN = ( t.first == maxntrkid );
-            bthmd.energy = t.second.E;
-            bthmd.numElectrons = t.second.NumElectrons;
-            MCPartHitassn->addSingle(hitPtr, mcpartPtr, bthmd);
-        }
-    
-    }//end loop on hits
+    fHitParticleAssociations->CreateHitParticleAssociations(evt, MCPartHitassn.get());
 
     evt.put(std::move(MCPartHitassn));
 } // Produce
