c330a38bbaf613383c7efa37d7002dc360fe9e06
Giuseppe Cerati <cerati@fnal.gov>
2018-11-29 11:57:16 -0600

    fix fMinTickDrift bug

--- larana/CosmicRemoval/CosmicTrackTagger_module.cc
+++ larana/CosmicRemoval/CosmicTrackTagger_module.cc
@@ -398,8 +398,7 @@ void cosmic::CosmicTrackTagger::reconfigure(fhicl::ParameterSet const & p) {
 
   //std::cerr << "Drift velocity is " << driftVelocity << " cm/us.  Sampling rate is: "<< fSamplingRate << " detector width: " <<  2*geo->DetHalfWidth() << std::endl;
   fDetectorWidthTicks = 2*geo->DetHalfWidth()/(driftVelocity*fSamplingRate/1000); // ~3200 for uB
-  fMinTickDrift = p.get("MinTickDrift", 3200);
-  fMinTickDrift = ts->TPCTDC2Tick(0.); //ts->TriggerOffsetTPC() * -1.);
+  fMinTickDrift = ts->Time2Tick(ts->TriggerTime());
   fMaxTickDrift = fMinTickDrift + fDetectorWidthTicks + fEndTickPadding;
 }
 
