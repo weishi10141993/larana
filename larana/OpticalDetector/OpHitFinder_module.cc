c91de0d0e115f54676c91245a37593f51947d978
Fran Nicolas <franciscojaviern1997@gmail.com>
2022-07-21 10:48:46 -0500

    Modified pulse finder constructors and add fUseStartTime

--- larana/OpticalDetector/OpHitFinder_module.cc
+++ larana/OpticalDetector/OpHitFinder_module.cc
@@ -35,6 +35,7 @@
 #include "art/Framework/Core/ModuleMacros.h"
 #include "art/Framework/Principal/Event.h"
 #include "art/Framework/Principal/Handle.h"
+#include "art/Utilities/make_tool.h"
 #include "canvas/Utilities/Exception.h"
 #include "fhiclcpp/ParameterSet.h"
 
@@ -73,6 +74,7 @@ namespace opdet {
 
     Float_t fHitThreshold;
     unsigned int fMaxOpChannel;
+    bool fUseStartTime;
 
     calib::IPhotonCalibrator const* fCalib = nullptr;
   };
@@ -93,6 +95,7 @@ namespace opdet {
     fInputModule = pset.get<std::string>("InputModule");
     fGenModule = pset.get<std::string>("GenModule");
     fInputLabels = pset.get<std::vector<std::string>>("InputLabels");
+    fUseStartTime = pset.get<bool>("UseStartTime", false);
 
     for (auto const& ch :
          pset.get<std::vector<unsigned int>>("ChannelMasks", std::vector<unsigned int>()))
@@ -124,22 +127,50 @@ namespace opdet {
       fCalib = new calib::PhotonCalibratorStandard(SPEArea, SPEShift, areaToPE);
     }
 
+    // Initialize the rise time calculator tool
+    using RTC = pmtana::RiseTimeCalculatorBase;
+    using PS = fhicl::ParameterSet;
+
+    PS rise_alg_pset;
+    bool computeRiseTime = pset.get_if_present< PS >("RiseTimeCalculator", rise_alg_pset);
+
     // Initialize the hit finder algorithm
     auto const hit_alg_pset = pset.get<fhicl::ParameterSet>("HitAlgoPset");
     std::string threshAlgName = hit_alg_pset.get<std::string>("Name");
-    if (threshAlgName == "Threshold")
-      fThreshAlg = new pmtana::AlgoThreshold(hit_alg_pset);
-    else if (threshAlgName == "SiPM")
-      fThreshAlg = new pmtana::AlgoSiPM(hit_alg_pset);
-    else if (threshAlgName == "SlidingWindow")
-      fThreshAlg = new pmtana::AlgoSlidingWindow(hit_alg_pset);
-    else if (threshAlgName == "FixedWindow")
-      fThreshAlg = new pmtana::AlgoFixedWindow(hit_alg_pset);
-    else if (threshAlgName == "CFD")
-      fThreshAlg = new pmtana::AlgoCFD(hit_alg_pset);
-    else
+    if (threshAlgName == "Threshold"){
+      if(computeRiseTime)
+        fThreshAlg = new pmtana::AlgoThreshold(hit_alg_pset, art::make_tool<RTC>(rise_alg_pset));
+      else
+        fThreshAlg = new pmtana::AlgoThreshold(hit_alg_pset, nullptr);
+    }
+    else if (threshAlgName == "SiPM"){
+      if(computeRiseTime)
+        fThreshAlg = new pmtana::AlgoSiPM(hit_alg_pset, art::make_tool<RTC>(rise_alg_pset));
+      else
+        fThreshAlg = new pmtana::AlgoSiPM(hit_alg_pset, nullptr);
+    }
+    else if (threshAlgName == "SlidingWindow"){
+      if(computeRiseTime)
+        fThreshAlg = new pmtana::AlgoSlidingWindow(hit_alg_pset, art::make_tool<RTC>(rise_alg_pset));
+      else
+        fThreshAlg = new pmtana::AlgoSlidingWindow(hit_alg_pset, nullptr);
+    }
+    else if (threshAlgName == "FixedWindow"){
+      if(computeRiseTime)
+        fThreshAlg = new pmtana::AlgoFixedWindow(hit_alg_pset, art::make_tool<RTC>(rise_alg_pset));
+      else
+        fThreshAlg = new pmtana::AlgoFixedWindow(hit_alg_pset, nullptr);
+    }
+    else if (threshAlgName == "CFD"){
+      if(computeRiseTime)
+        fThreshAlg = new pmtana::AlgoCFD(hit_alg_pset, art::make_tool<RTC>(rise_alg_pset));
+      else
+        fThreshAlg = new pmtana::AlgoCFD(hit_alg_pset, nullptr);
+    }
+    else{
       throw art::Exception(art::errors::UnimplementedFeature)
         << "Cannot find implementation for " << threshAlgName << " algorithm.\n";
+    }
 
     auto const ped_alg_pset = pset.get<fhicl::ParameterSet>("PedAlgoPset");
     std::string pedAlgName = ped_alg_pset.get<std::string>("Name");
@@ -207,7 +238,8 @@ namespace opdet {
                    geometry,
                    fHitThreshold,
                    clock_data,
-                   calibrator);
+                   calibrator,
+                   fUseStartTime);
     }
     else {
 
@@ -243,7 +275,8 @@ namespace opdet {
                    geometry,
                    fHitThreshold,
                    clock_data,
-                   calibrator);
+                   calibrator,
+                   fUseStartTime);
     }
     // Store results into the event
     evt.put(std::move(HitPtr));
