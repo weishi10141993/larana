d848351fff71e9938cab23d8259724aa3a9f092b
Rodrigo Alvarez Garrote <70945401+rodralva@users.noreply.github.com>
2022-12-13 12:19:49 +0100

    Simple tool added:     Instead of using rise time, this tool looks for the first maximum     of an optical signal and fits the peak to a gaussian.

---
+++ larana/OpticalDetector/OpHitFinder/RiseTimeTools/RiseTimeGaussFit_tool.cc
@@ -0,0 +1,149 @@
+/**
+ * \file RiseTimeGaussFit_tool.cc
+ *
+ * \brief Gaussian fit method implemented to  compute Rise time as
+ * the value of the center of the first local maximum. 
+ * Fixed min threshold required set in the fhicl file
+ *
+ * @author Rodrigo Alvarez, Sept 2022
+ */
+
+#include "fhiclcpp/types/Atom.h"
+#include "art/Utilities/ToolMacros.h"
+#include "art/Utilities/ToolConfigTable.h"
+
+#include "RiseTimeCalculatorBase.h"
+
+// ROOT includes
+#include "TF1.h"
+#include "TH1F.h"
+
+namespace pmtana{
+
+  class RiseTimeGaussFit : RiseTimeCalculatorBase
+  {
+
+  public:
+
+    //Configuration parameters
+    struct Config {
+
+      fhicl::Atom<double> MinAmp {
+        fhicl::Name("MinAmp")
+      };
+      fhicl::Atom<double> InitSigma {
+        fhicl::Name("InitSigma")
+      };
+      fhicl::Atom<int> Nbins {
+        fhicl::Name("Nbins")
+      };
+      fhicl::Atom<double> Tolerance {
+        fhicl::Name("Tolerance")
+      };
+
+    };
+
+    // Default constructor
+    explicit RiseTimeGaussFit(art::ToolConfigTable<Config> const& config);
+
+    // Method to calculate the OpFlash t0
+    double RiseTime(const pmtana::Waveform_t& wf_pulse,
+                    const pmtana::PedestalMean_t& ped_pulse,
+                    bool _positive) const override;
+    // Method to fit the first local max of the wvf above fixed threshold
+    int findFirstMax(const std::vector<double>& arr, 
+                     int low, 
+                     int high,
+                     double threshold) const;
+
+  private:
+
+    double fMinAmp;
+    double fInitSigma;
+    double fTolerance;
+    int    fNbins;
+
+  };
+
+  RiseTimeGaussFit::RiseTimeGaussFit(art::ToolConfigTable<Config> const& config)
+    : fMinAmp { config().MinAmp() }
+    , fInitSigma {config().InitSigma()}
+    , fTolerance {config().Tolerance()}
+    , fNbins {config().Nbins()}
+  {
+  }
+
+  double RiseTimeGaussFit::RiseTime(const pmtana::Waveform_t& wf_pulse,
+                                     const pmtana::PedestalMean_t& ped_pulse,
+                                     bool _positive) const{
+
+    // Pedestal-subtracted pulse
+    std::vector<double> wf_aux (ped_pulse);
+    if(_positive){
+      for(size_t ix=0; ix<wf_aux.size(); ix++){
+        wf_aux[ix]=((double)wf_pulse[ix])-wf_aux[ix];
+      }
+    }
+    else{
+      for(size_t ix=0; ix<wf_aux.size(); ix++){
+        wf_aux[ix]=wf_aux[ix]-((double)wf_pulse[ix]);
+      }
+    }
+
+    // Find first local maximum
+    size_t first_max = findFirstMax(wf_aux,0,wf_aux.size(), fMinAmp);
+    
+    // Create & fill th1
+    TH1F *h_aux = new TH1F("aux", "aux", wf_aux.size(), -0.5, wf_aux.size() - 0.5);
+    for (long unsigned int j = 0; j < wf_aux.size(); j++) h_aux->SetBinContent(j+1,wf_aux[j]);
+    
+    // Function & initial values for the fit, ROOT Gaussian:  [p0]*e**(-0.5 (x-[p1])**2 / [p2]**2)
+    TF1 *f = new TF1("f","gaus",double(first_max)-fNbins,double(first_max)+fNbins);
+    std::vector<double> p_init={wf_aux[first_max],double(first_max),fInitSigma};
+    for (long unsigned int p = 0; p < wf_aux.size(); p++) f->SetParameter(p,p_init[p]);
+
+    // Fit 
+    h_aux->Fit(f,"q","SAME",first_max-fNbins,first_max+fNbins);
+    double t_fit=f->GetParameter(1);
+    double peak_time;
+    if (std::abs(t_fit-first_max)<fTolerance)
+    {//check fit is close in distance to the original max, use max peak as time otherwise
+      peak_time=t_fit;
+    }
+    else
+    {
+      peak_time=first_max;
+      std::cout<<"No good fit found, keeping 1st max bin instead"<<std::endl;
+    }
+    // double peak_time= (std::abs(t_fit-first_max)<2) ?  t_fit:first_max;
+
+
+    return peak_time;
+  }
+
+  int RiseTimeGaussFit::findFirstMax(const std::vector<double>& arr, int low, int high,double threshold=4.) 
+  const{/** 
+    * Linear search, O(N), 1st peak should be close to the start of the vector 
+    * for scintillation LAr Signals. Returns the position of the first local max
+    **/ 
+    double max = arr[low];
+    int i;
+    for (i = low + 1; i < high; i++)
+    {
+        if (arr[i] >= max) 
+            max = arr[i];
+        
+        else if(max<threshold) //didn't pass the threshold, keep searching
+            max = arr[i];
+        
+        else
+            return i-1;
+    }
+
+    //No local maxima found.
+    std::cout<<"No local max found above fixed threshold: "<< threshold<<std::endl;
+    return 0;
+  }
+}
+
+DEFINE_ART_CLASS_TOOL(pmtana::RiseTimeGaussFit)
